db860f8755cccf539d4b3219a92d2d9c472ff827
patrick <pemmerich@intstrux.com>
2014-04-24 15:40:52 -0400

    made small changes

--- src/js/Main.js
+++ src/js/Main.js
@@ -64,7 +64,7 @@ function init()
 	$(thumbnails).each(function (i,elem) {
 		  //console.log(" build thumb "+i+" "+elem.title);
 		  $('#thumb_scroll_container > .scroller > .list').append('<li><div class="thumb_container" id="thumb_container_'+i+'"><a href="'+elem.full_file+'" id=thumb_anchor_"'+i+'" title="'+elem.title+'"><img src="'+elem.thumb_file+'"></a></div></li>');
-		  var translateY = Math.floor(Math.random() * 50) - 25;
+		  var translateY = Math.floor(Math.random() * 50) - 10;
 		  $('#thumb_container_'+i+' > a > img').css({"-webkit-transform":"translateY("+translateY+"px) rotateX(60deg) rotateZ(35deg)","transform":"translateY("+translateY+"px) rotateX(60deg) rotateZ(35deg)"});
 		 
 	  });
